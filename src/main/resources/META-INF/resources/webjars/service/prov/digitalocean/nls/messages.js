14b4041e934632866074e4a0bdffe87986f407ea
Fabrice Daugan <fabrice.daugan@gmail.com>
2020-03-26 22:40:24 +0100

    Update digital ocean icons

---
+++ src/main/resources/META-INF/resources/webjars/service/prov/digitalocean/nls/messages.js
@@ -0,0 +1,14 @@
+/*
+ * Licensed under MIT (https://github.com/ligoj/ligoj/blob/master/LICENSE)
+ */
+define({
+	"root" : {
+		"service:prov:digitalocean:access-key" : "Application Key",
+		"service:prov:digitalocean:name" : "Name",
+		"error" : {
+			"digitalocean-login" : "Authentication failed",
+			"digitalocean-admin" : "Administrator access failed"
+		}
+	},
+	"fr" : true
+});
